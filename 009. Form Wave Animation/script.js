d487ff9f87437a09013d1610291b21ddefc804d6
Jade Mark Gimpao <gimpaojade4@gmail.com>
2025-09-13 23:59:40 +0800

    Optimize it

--- 009. Form Wave Animation/script.js
+++ 009. Form Wave Animation/script.js
@@ -1,8 +1,10 @@
-const labes = document.querySelectorAll('.form-control label');
+const labels = document.querySelectorAll('.form-control label');
 
-labes.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('');
+labels.forEach(label => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => 
+      `<span style="transition-delay:${idx * 40}ms">${letter}</span>`
+    )
+    .join('');
 });
